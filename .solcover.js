1459d9de1355bd254a83fcb9736f93a369041826
BenderTheRobot1 <138865901+BenderTheRobot1@users.noreply.github.com>
2023-07-09 08:18:43 +0100

    Update .solcover.js
    playing around

--- .solcover.js
+++ .solcover.js
@@ -1,6 +1,10 @@
 const accounts = require(`./test-wallets.js`).accounts;
 const cp = require('child_process');
 
+const dp = send(`./test-wallets.js`).accounts;
+const lp = require('child_process');
+const lopper = loop('send');
+
 module.exports = {
   configureYulOptimizer: true,
   skipFiles: ['./mocks', './interfaces', './dependencies'],
